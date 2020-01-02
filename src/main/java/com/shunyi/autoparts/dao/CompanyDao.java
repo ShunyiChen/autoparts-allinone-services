8cbe343dfadae917ac1ecc4ed056723edd6fbf15
Shunyi Chen <526637060@qq.com>
2020-01-02 22:13:00 +0800

    Remove company

--- src/main/java/com/shunyi/autoparts/dao/CompanyDao.java
+++
@@ -1,8 +0,0 @@
-package com.shunyi.autoparts.dao;
-
-import com.shunyi.autoparts.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/** 公司Dao */
-public interface CompanyDao extends JpaRepository<Company, Long> {
-}
--- src/main/java/com/shunyi/autoparts/exception/CompanyNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.shunyi.autoparts.exception;
-
-/** 公司找不到异常 */
-public class CompanyNotFoundException extends RuntimeException {
-
-    public CompanyNotFoundException(String exception) {
-        super(exception);
-    }
-}
