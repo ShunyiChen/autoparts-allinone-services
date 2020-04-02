42bc4918b8b840d597afc842c5ed0a605209dcac
Chen <71604909@HS.DL>
2020-04-02 13:36:15 +0800

    production place updates

--- src/main/java/com/shunyi/autoparts/controller/PlaceController.java
+++ src/main/java/com/shunyi/autoparts/controller/PlaceController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 /**
  * @description 配件产地控制器
  * @author Shunyi Chen
- * @date 2020/3/23
+ * @date 2020/4/2
  */
 @RestController
 @CrossOrigin
@@ -27,13 +27,13 @@ public class PlaceController {
     @Autowired
     private PlaceDao placeDao;
 
-    @PostMapping("/cars")
+    @PostMapping("/places")
     public ResponseEntity<?> create(@RequestBody Place place) {
         Place savedCar = placeDao.save(place);
         return new ResponseEntity<>(savedCar.getId(), HttpStatus.OK);
     }
 
-    @PutMapping("/cars/{id}")
+    @PutMapping("/places/{id}")
     public ResponseEntity<?> update(@RequestBody Place place, @PathVariable Long id) {
         Optional<Place> carOptional = placeDao.findById(id);
         if (!carOptional.isPresent()) {
@@ -44,17 +44,17 @@ public class PlaceController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/cars/{id}")
+    @DeleteMapping("/places/{id}")
     public void delete(@PathVariable Long id) {
         placeDao.deleteById(id);
     }
 
-    @GetMapping("/cars")
+    @GetMapping("/places")
     public List<Place> retrieveAll() {
         return placeDao.findAll(Sort.by(Sort.Direction.ASC, "id"));
     }
 
-    @GetMapping("/cars/{id}")
+    @GetMapping("/places/{id}")
     public Place retrieve(@PathVariable Long id) {
         Optional<Place> place = placeDao.findById(id);
         if (!place.isPresent()) {
